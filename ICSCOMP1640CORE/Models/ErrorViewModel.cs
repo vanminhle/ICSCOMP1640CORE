90ff52d9c49d87c5aba38ef7b159fc129ef9cc24
vanminhle <vanminh.le.754@gmail.com>
2022-03-04 13:03:14 +0700

    Add project files.

---
+++ ICSCOMP1640CORE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ICSCOMP1640CORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
