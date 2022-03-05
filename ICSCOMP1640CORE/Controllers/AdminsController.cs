794792ca6eee5a195c89591cf5e3657313a4e6b3
Than Minh Hy <thanminhhy@gmail.com>
2022-03-05 20:11:31 +0700

    Create Department features

---
+++ ICSCOMP1640CORE/Controllers/AdminsController.cs
@@ -0,0 +1,52 @@
+﻿using ICSCOMP1640CORE.Data;
+using ICSCOMP1640CORE.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ICSCOMP1640CORE.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public AdminsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var departments = _db.Departments.ToList();
+            return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult CreateDepartment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateDepartment(Department model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var existDepartment = _db.Departments.Any(x => x.DepartmentName == model.DepartmentName);
+            if (existDepartment == true)
+            {
+                ModelState.AddModelError("", "Department Already Exists.");
+                return View(model);
+            }
+            var newDepartment = new Department()
+            {
+                DepartmentName = model.DepartmentName,
+                Description = model.Description,
+            };
+            _db.Departments.Add(newDepartment);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
