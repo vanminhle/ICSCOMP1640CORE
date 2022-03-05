9db0146567060be556a93b6ded0fbfdee512b4d3
vanminhle <vanminh.le.754@gmail.com>
2022-03-05 19:35:02 +0700

    Fix Page Login Title

---
+++ ICSCOMP1640CORE/Controllers/AdminsController.cs
@@ -0,0 +1,35 @@
+﻿using ICSCOMP1640CORE.Data;
+using ICSCOMP1640CORE.Models;
+using ICSCOMP1640CORE.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICSCOMP1640CORE.Controllers
+{
+    [Authorize(Roles = Role.Admin)]
+    public class AdminsController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private ApplicationDbContext _db;
+        public AdminsController(ApplicationDbContext db, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Manage account of each QA Coordinator Departments
+
+
+    }
+}
