8cc9b4880e435bf726962309ba10713b29c5fbc5
anhhuy0168 <anhhuy123zxc123@gmail.com>
2022-03-17 13:39:20 +0700

    SPRINT-3 T01-R13 T01-R14  T02-R14 T01-R19 T02-R19 DONE

--- ICSCOMP1640CORE/Controllers/StaffsController.cs
+++ ICSCOMP1640CORE/Controllers/StaffsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -232,5 +233,6 @@ namespace ICSCOMP1640CORE.Controllers
             _notyf.Success("Comment is created successfully.");
             return RedirectToAction("IdeaIndex");
         }
+        
     }
 }
